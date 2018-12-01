8e08d437060562df4e85b7270fa5854c1da317ad
Clayton <clayton.lee411@gmail.com>
2018-12-01 12:31:11 -0500

    added MealType Model Class

---
+++ RecipeTastic/Models/MealType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTastic.Models
+{
+    public class MealType
+    {
+        public int MealTypeID { get; set; }
+        public string MealTypeName { get; set; }
+    }
+}
