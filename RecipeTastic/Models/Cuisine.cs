098d10d6f71e26388718037a7afb6e639543ca4f
Shalini <shalinisinghus@yahoo.com>
2018-12-01 12:20:29 -0500

    Added Cuisine Model class

---
+++ RecipeTastic/Models/Cuisine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTastic.Models
+{
+    public class Cuisine
+    {
+        public int CuisineId { get; set; }
+        public string CuisineName { get; set; }
+    }
+}
