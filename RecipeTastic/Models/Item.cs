7117927adb6b06a680faecb195ce050f6d4bfc01
Dominic <Dominic@gmail.com>
2018-12-01 13:54:45 -0500

    add item model class

---
+++ RecipeTastic/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeTastic.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        
+        public Cuisine Cuisine { get; set; }
+        public int CuisineId { get; set; }
+
+        public MealType MealType { get; set; }
+        public int MealTypeId { get; set; }
+
+    }
+}
+
+
+
+    
