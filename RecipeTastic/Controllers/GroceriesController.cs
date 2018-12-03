3264dc4ad57275fd7abe57a3cbf594bb15e265be
vijijay <mail2vijijay@gmail.com>
2018-12-03 16:27:08 -0500

    Added Grocery List module

---
+++ RecipeTastic/Controllers/GroceriesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using RecipeTastic.Models;
+
+namespace RecipeTastic.Controllers
+{
+    public class GroceriesController : Controller
+    {
+        private ApplicationDbContext _Context;
+        public GroceriesController()
+        {
+            _Context = new ApplicationDbContext();
+
+        }
+        public ActionResult Index()
+        {
+            var Groceries = _Context.Groceries.ToList().Where(x => x.UserId == HttpContext.User.Identity.GetUserName());
+            return View(Groceries);
+
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+        public ActionResult Add(Grocery grocery)
+        {
+            _Context.Groceries.Add(grocery);
+
+            _Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var grocery = _Context.Groceries.SingleOrDefault(g => g.GroceryId == id);
+            if (grocery == null) return HttpNotFound(); return View(grocery);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Grocery Grocery)
+        {
+            var groceryInDb = _Context.Groceries.SingleOrDefault(g => g.GroceryId == Grocery.GroceryId);
+            if (groceryInDb == null) return HttpNotFound();
+            groceryInDb.GroceryName = Grocery.GroceryName;
+            groceryInDb.GroceryComment = Grocery.GroceryComment;
+            _Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var grocery = _Context.Groceries.SingleOrDefault(g => g.GroceryId == id);
+            if (grocery == null) return HttpNotFound();
+            return View(grocery);
+        }
+
+        [HttpPost]
+        public ActionResult DoDelete(Grocery Grocery)
+        {
+            var grocery = _Context.Groceries.SingleOrDefault(g => g.GroceryId == Grocery.GroceryId);
+            if (grocery == null)
+                return HttpNotFound();
+            _Context.Groceries.Remove(grocery);
+            _Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
