d067728d0d799f706874ec1d8e198733ee87a827
Shalini <shalinisinghus@yahoo.com>
2018-12-04 19:03:13 -0500

    Added MealPlanner Module

---
+++ RecipeTastic/Controllers/MealPlansController.cs
@@ -0,0 +1,32 @@
+﻿using RecipeTastic.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace RecipeTastic.Controllers
+{
+    public class MealPlansController : Controller
+    {
+        private ApplicationDbContext _context;
+        public MealPlansController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: MealPlans
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetItems()
+        {
+            List<Item> items = new List<Item>();
+            items = _context.Items.Include(i => i.MealType).ToListAsync().Result;
+            return Json(items, JsonRequestBehavior.AllowGet);
+
+
+        }
+    }
+}
